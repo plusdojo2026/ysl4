3598fde94d2543c804d000da5b50a499f67354ea
YSLyokota <miho.yokota@ysl.co.jp>
2026-07-17 17:59:02 +0900

    Update TaskDAO.java

--- source/src/main/java/DAO/TaskDAO.java
+++ source/src/main/java/DAO/TaskDAO.java
@@ -17,8 +17,45 @@ public class TaskDAO {
 		this.conn = conn;
 	}
 
-	//タスクの一覧を取得するメソッド
+	//タスクの一覧を取得する
 	public ArrayList<TaskDTO> selectAll() throws SQLException {
+		ArrayList<TaskDTO> TaskList = new ArrayList<TaskDTO>();
+
+		// SELECT文の下準備
+		String sql = "SELECT * FROM Tasks";
+		//デバッグ（SQL文の確認用）
+		System.out.println(sql);
+
+		// コネクションとSQLをまとめる
+		PreparedStatement pStmt = conn.prepareStatement(sql);
+
+		// SELECT文を実行(executeQuery)し、結果表をResultSetに取得する
+		ResultSet rs = pStmt.executeQuery();
+
+		//reはDAOでしか使えないのでArrayList移し替え
+		while (rs.next()) {
+			TaskDTO dto = new TaskDTO();
+			dto.setTaskId(rs.getInt("task_id"));
+			dto.setTaskName(rs.getString("task_name"));
+			dto.setProjectId(rs.getInt("project_id"));
+			dto.setManagerId(rs.getInt("manager_id"));
+			dto.setStartDate(rs.getDate("start_date"));
+			dto.setDueDate(rs.getDate("due_date"));
+			dto.setEstimatedManhours(rs.getInt("estimated_manhours"));
+			dto.setProgress(rs.getInt("progress"));
+			dto.setStatus(rs.getString("status"));
+			dto.setPriority(rs.getString("priority"));
+			dto.setDescription(rs.getString("description"));
+			dto.setCreatedAt(rs.getTimestamp("c_at"));
+			dto.setUpdatedAt(rs.getTimestamp("u_at"));
+			TaskList.add(dto);
+		}
+		//serviceに返却する
+		return TaskList;
+	}
+
+	//案件一覧を取得する
+	public ArrayList<TaskDTO> selectByProjectId() throws SQLException {
 		ArrayList<TaskDTO> TaskList = new ArrayList<TaskDTO>();
 
 		// SELECT文を準備する
@@ -52,6 +89,85 @@ public class TaskDAO {
 		}
 		//serviceに返却する
 		return TaskList;
+	}
+
+	//検索のための一覧を取得する
+	public ArrayList<TaskDTO> search() throws SQLException {
+		ArrayList<TaskDTO> TaskList = new ArrayList<TaskDTO>();
+
+		// SELECT文を準備する
+		String sql = "SELECT * FROM Tasks";
+		//デバッグ（SQL文の確認用）
+		System.out.println(sql);
+
+		// まとめる
+		PreparedStatement pStmt = conn.prepareStatement(sql);
+
+		// SELECT文を実行し、結果表を取得する
+		ResultSet rs = pStmt.executeQuery();
+
+		//移し替え
+		while (rs.next()) {
+			TaskDTO dto = new TaskDTO();
+			dto.setTaskId(rs.getInt("task_id"));
+			dto.setTaskName(rs.getString("task_name"));
+			dto.setProjectId(rs.getInt("project_id"));
+			dto.setManagerId(rs.getInt("manager_id"));
+			dto.setStartDate(rs.getDate("start_date"));
+			dto.setDueDate(rs.getDate("due_date"));
+			dto.setEstimatedManhours(rs.getInt("estimated_manhours"));
+			dto.setProgress(rs.getInt("progress"));
+			dto.setStatus(rs.getString("status"));
+			dto.setPriority(rs.getString("priority"));
+			dto.setDescription(rs.getString("description"));
+			dto.setCreatedAt(rs.getTimestamp("c_at"));
+			dto.setUpdatedAt(rs.getTimestamp("u_at"));
+			TaskList.add(dto);
+		}
+		//serviceに返却する
+		return TaskList;
+	}
+
+	//IDを取得する
+	public TaskDTO findById(int projectId) throws SQLException {
+
+		TaskDTO dto = null;
+
+		// SELECT文を準備する
+		String sql = "SELECT * FROM projects WHERE project_id=?";
+
+		// デバッグ（SQL文の確認用）
+		System.out.println(sql);
+
+		// まとめる
+		PreparedStatement pStmt = conn.prepareStatement(sql);
+		pStmt.setInt(1, projectId);
+
+		// SELECT文を実行し、結果表を取得する
+		ResultSet rs = pStmt.executeQuery();
+
+		// 移し替え
+		while (rs.next()) {
+
+			dto = new TaskDTO();
+			dto.setTaskId(rs.getInt("task_id"));
+			dto.setTaskName(rs.getString("task_name"));
+			dto.setProjectId(rs.getInt("project_id"));
+			dto.setManagerId(rs.getInt("manager_id"));
+			dto.setStartDate(rs.getDate("start_date"));
+			dto.setDueDate(rs.getDate("due_date"));
+			dto.setEstimatedManhours(rs.getInt("estimated_manhours"));
+			dto.setProgress(rs.getInt("progress"));
+			dto.setStatus(rs.getString("status"));
+			dto.setPriority(rs.getString("priority"));
+			dto.setDescription(rs.getString("description"));
+			dto.setCreatedAt(rs.getTimestamp("c_at"));
+			dto.setUpdatedAt(rs.getTimestamp("u_at"));
+
+		}
+
+		// serviceに返却する
+		return dto;
 	}
 
 }
