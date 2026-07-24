794f53ab4db47119527b8804e9f458ca71cd36bf
YSLyokota <miho.yokota@ysl.co.jp>
2026-07-24 14:22:43 +0900

    Update TaskAction.java

--- source/src/main/java/action/TaskAction.java
+++ source/src/main/java/action/TaskAction.java
@@ -1,6 +1,7 @@
 package action;
 
 import java.io.UnsupportedEncodingException;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 import javax.servlet.http.HttpServletRequest;
@@ -21,49 +22,107 @@ public class TaskAction {
 	public String list() throws UnsupportedEncodingException {
 
 		String page = "/WEB-INF/jsp/taskList.jsp";
+
 		TaskService service = new TaskService();
 
 		//タスク情報を全て取得する
 		TaskService selectservice = new TaskService();
+
 		ArrayList<TaskDTO> taskList = selectservice.selectAll();
+
 		request.setAttribute("taskList", taskList);
 
 		return page;
 	}
+
+	//キーワード検索
+	public String search() throws UnsupportedEncodingException {
+		String page = "/WEB-INF/jsp/taskList.jsp";
+
+		//ユーザーが操作した情報を受け取る
+		String keyword = request.getParameter("keyword");
+
+		//Serviceに接続する 一覧所得
+		//TaskService service = new TaskService();
+
+		//検索語の一覧表示
+		TaskService searchservice = new TaskService();
+		//				ArrayList<TaskDTO> taskList = searchservice.search(0);
+		//				request.setAttribute("taskList", taskList);
+		request.setAttribute("keyword", keyword);
+		return page;
+	}
+
+	//　！！キーワード検索メモ！！
+//	public String search() throws UnsupportedEncodingException {
+//		String page = "/WEB-INF/jsp/taskList.jsp";
+//
+//		//ユーザーが操作した情報を受け取る
+//		String keyword = request.getParameter("keyword");
+//
+//		//Serviceに接続する 一覧所得
+//		//TaskService service = new TaskService();
+//
+//		//検索語の一覧表示
+//		TaskService searchservice = new TaskService();
+//		//				ArrayList<TaskDTO> taskList = searchservice.search(0);
+//		//				request.setAttribute("taskList", taskList);
+//		request.setAttribute("keyword", keyword);
+//		return page;
+//	}
 
 	//プロジェクトID検索
 	public String selectByProjectId() throws UnsupportedEncodingException {
 		String page = "/WEB-INF/jsp/taskList.jsp";
-		
+
 		//ユーザーが操作した情報を受け取る
 		String keyword = request.getParameter("keyword");
 		
-		//Serviceに接続する
-		//TaskService service = new TaskService();
-		
 		TaskService searchservice = new TaskService();
+		
 		ArrayList<TaskDTO> taskList = searchservice.selectByProjectId(0);
+		
 		request.setAttribute("taskList", taskList);
+		
 		request.setAttribute("keyword", keyword);
 		return page;
 	}
-	
+
 	//タスクID検索
-		public String search() throws UnsupportedEncodingException {
-			String page = "/WEB-INF/jsp/taskList.jsp";
-			
-			//ユーザーが操作した情報を受け取る
-			String keyword = request.getParameter("keyword");
-			
-			//Serviceに接続する 一覧所得
-			//TaskService service = new TaskService();
-			
-			//検索語の一覧表示
-			TaskService searchservice = new TaskService();
-//			ArrayList<TaskDTO> taskList = searchservice.findById(0);
-//			request.setAttribute("taskList", taskList);
-			request.setAttribute("keyword", keyword);
-			return page;
+	public String findDetail() throws UnsupportedEncodingException {
+		String page = "/WEB-INF/jsp/taskList.jsp";
+
+		//ユーザーが操作した情報を受け取る
+		String keyword = request.getParameter("keyword");
+
+		//検索語の一覧表示
+		TaskService searchservice = new TaskService();
+		
+		request.setAttribute("keyword", keyword);
+		return page;
+	}
+
+	//詳細詳細
+	public String detail() {
+
+		String page = "/WEB-INF/jsp/taskDetail.jsp";
+
+		int taskId = Integer.parseInt(
+				request.getParameter("taskId"));
+
+		TaskService service = new TaskService();
+
+		TaskDTO task = null;
+		try {
+			task = service.findDetail(taskId);
+		} catch (SQLException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
 		}
+
+		request.setAttribute("task", task);
+
+		return page;
+	}
 
 }
