aa84a362e175aaa7ad731bcef4ed832ba9c06bf9
YSLyokota <miho.yokota@ysl.co.jp>
2026-07-24 17:47:01 +0900

    とりあえず出来た！

--- source/src/main/java/action/TaskAction.java
+++ source/src/main/java/action/TaskAction.java
@@ -47,9 +47,10 @@ public class TaskAction {
 
 		//検索語の一覧表示
 		TaskService searchservice = new TaskService();
+		ArrayList<TaskDTO> taskList = searchservice.search(keyword);
 		//				ArrayList<TaskDTO> taskList = searchservice.search(0);
 		//				request.setAttribute("taskList", taskList);
-		request.setAttribute("keyword", keyword);
+		request.setAttribute("taskList", taskList);
 		return page;
 	}
 
@@ -126,23 +127,125 @@ public class TaskAction {
 
 	// 登録画面表示
 	public String showRegist() {
-		
+
 		return "/WEB-INF/jsp/taskRegist.jsp";
-		
+
 	}
-	
+
 	//タスク登録
 	public String regist() throws UnsupportedEncodingException {
-		String page = "/WEB-INF/jsp/menu.jsp";
+		String page = "/WEB-INF/jsp/projectDetail.jsp";
 
 		//値の取得
 		request.setCharacterEncoding("UTF-8");
+		String projectId = request.getParameter("projectId");
 		String taskName = request.getParameter("taskName");
-		String estimateHours = request.getParameter("estimateHours");
 		String status = request.getParameter("status");
 		String priority = request.getParameter("priority");
-		
 
+		//入力値チェック
+		if (projectId == null || taskName == null || status == null || priority == null) {
+			request.setAttribute("msg", "※必須項目を入力してください");
+		}
+
+		TaskDTO dto = new TaskDTO();
+
+		dto.setProjectId(Integer.parseInt(projectId));
+		dto.setTaskName(taskName);
+		dto.setStatus(status);
+		dto.setPriority(priority);
+
+		TaskService service = new TaskService();
+
+		//serviceに処理を依頼
+		int ans = service.regist(dto);
+
+		//ちゃんと登録できたか確認
+		if (ans == 1) {
+			request.setAttribute("msg", "※" + taskName + "の登録完了！");
+		} else {
+			request.setAttribute("msg", "※登録失敗！IDが重複しています");
+		}
+
+		return page;
+	}
+
+	//編集内容を表示する
+	public String showUpdate() {
+
+		String page = "/WEB-INF/jsp/projectEdit.jsp";
+
+		int taskId = Integer.parseInt(
+				request.getParameter("taskName"));
+
+		TaskService service = new TaskService();
+
+		TaskDTO dto = service.findById(taskId);
+
+		request.setAttribute("dto", dto);
+		return page;
+
+	}
+
+	//編集
+	public String update() throws UnsupportedEncodingException {
+
+		String page = "/WEB-INF/jsp/projectDetail.jsp";
+
+		request.setCharacterEncoding("UTF-8");
+
+		TaskDTO dto = new TaskDTO();
+
+		dto.setTaskId(Integer.parseInt(request.getParameter("task_id")));
+		dto.setTaskName(request.getParameter("task_name"));
+		dto.setStatus(request.getParameter("task_status"));
+		dto.setPriority(request.getParameter("task_priority"));
+
+		TaskService service = new TaskService();
+
+		int ans = service.update(dto);
+
+		if (ans == 1) {
+			page = "/TaskServlet?action=detail"
+					+ "&taskId="
+					+ dto.getTaskId();
+		}
+
+		return page;
+	}
+
+	//ステータス変更
+	public String changeStatus() {
+
+		String page = "/TaskServlet?action=list";
+
+		int taskId = Integer.parseInt(request.getParameter("taskId"));
+
+		String status = request.getParameter("status");
+		
+		int progres = Integer.parseInt(request.getParameter("progres"));
+
+		TaskService service = new TaskService();
+
+		service.changeStatus(taskId, status, progres);
+
+		return page;
+	}
+
+	//削除
+	public String delete() throws UnsupportedEncodingException {
+		String page = "/WEB-INF/jsp/projectDetail.jsp";
+
+		//値の取得
+		request.setCharacterEncoding("UTF-8");
+		String id = request.getParameter("delete_button");
+
+		//入力値チェック
+		if (id == null) {
+			request.setAttribute("msg", "IDが入力されていません。");
+		}
+
+		return page;
 	}
 
 }
